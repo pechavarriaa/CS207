64998efd67d065b62d1c7d111924291b0b3922ee
Pablo Echavarria <pechavarriaa@gmail.com>
2017-10-19 12:42:00 -0600

    Update LightSensor(Lab4).ino

--- LightSensor(Lab4).ino
+++ LightSensor(Lab4).ino
@@ -1,20 +1,22 @@
 /* 
- Playing with pots.
- Written 23 Aug 2011 by Alex Clarke
+ Pablo Echavarria
+ 200385648
+ 
+Constant Variables: ledPin(for write on led), lightPin(to read from light sensor)
+Variables:  light_value(reads from arduino based on the light)
  */
 
-//constants for this sketch
+//constant pins declarations
 const int ledPin = 11;
-const int potPin = A0;
+const int lightPin = A0;
 
-// variables for this sketch
-int pot_value;
-int frequency;
+// variables 
+int light_value;
 
 void setup()
 {
-  pinMode(ledPin, OUTPUT);
-  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);//set pin to output
+  Serial.begin(9600);//initialize serial Port
 }
 
 
@@ -22,15 +24,18 @@ void setup()
 void loop()
 {
   //read voltage from the potentiometer
-  pot_value = analogRead(potPin);
+  light_value = analogRead(lightPin);
 
-
-    Serial.println(pot_value );
+  //print to serial port to see the values
+    Serial.println( light_value);
    
-    if(pot_value < 200)
+ //conditions to check how dim the light will be
+    if(light_value < 200)
     analogWrite(ledPin, 255);
-    else if(pot_value < 500)
+ 
+    else if( light_value < 500)
     analogWrite(ledPin, 125);
+ 
     else
     analogWrite(ledPin,0);
 }
